e1df191a09ed5ffad080824e80d8b1f1554ec184
FMS-Cat <yutaka.obuchi@gmail.com>
2019-03-16 13:57:57 +0900

    cog.svg.js is now lint ready

--- src/images/utils/cog.svg.js
+++ src/images/utils/cog.svg.js
@@ -1,12 +1,12 @@
-let s = 100.0;
-let c = s / 2.0;
+const s = 100.0;
+const c = s / 2.0;
 for ( let i = 0; i < 24; i ++ ) {
-  let r = ( i & 2 ) === 0 ? 100 * 0.42 : 100 * 0.30;
-  let t = Math.PI * ( i - 0.5 ) / 12.0;
+  const r = ( i & 2 ) === 0 ? 100 * 0.42 : 100 * 0.30;
+  const t = Math.PI * ( i - 0.5 ) / 12.0;
 
-  console.log(
-    "L " +
-    Math.round( c + Math.cos( t ) * r ) + " " +
+  process.stdout.write(
+    'L ' +
+    Math.round( c + Math.cos( t ) * r ) + ' ' +
     Math.round( c + Math.sin( t ) * r )
   );
 }
