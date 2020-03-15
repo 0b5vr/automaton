6e339f752e51e89f10cffc3b2fb1a9431dcb0d61
FMS-Cat <yutaka.obuchi@gmail.com>
2020-03-16 00:01:46 +0900

    (from automaton-with-gui repo) fix useDoubleClick

--- src/view/utils/useDoubleClick.ts
+++ src/view/utils/useDoubleClick.ts
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useCallback, useRef } from 'react';
 
 export function useDoubleClick( interval = 250 ): () => boolean {
-  const [ lastClick, setLastClick ] = useState<number>( 0 );
+  const refLastClick = useRef( 0 );
 
-  return () => {
-    const date = Date.now();
-    const delta = date - lastClick;
-    setLastClick( date );
-    return ( delta < interval );
-  };
+  return useCallback(
+    () => {
+      const date = Date.now();
+      const delta = date - refLastClick.current;
+      refLastClick.current = date;
+      return ( delta < interval );
+    },
+    []
+  );
 }
