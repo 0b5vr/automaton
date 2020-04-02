722eb3ff1d87034a5de64f6d969800f1ed89800a
FMS-Cat <yutaka.obuchi@gmail.com>
2020-04-03 00:28:49 +0900

    oof

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './types';
 
-export { Automaton, AutomatonOptions } from './Automaton';
+export { Automaton } from './Automaton';
 export { Channel } from './Channel';
 export { ChannelItem } from './ChannelItem';
 export { ChannelItemConstant } from './ChannelItemConstant';
