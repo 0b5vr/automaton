daa18a29f8494c613be02473962b5b182f2aa1b2
FMS-Cat <yutaka.obuchi@gmail.com>
2020-04-05 21:59:10 +0900

    (from automaton-with-gui repo) DopeSheet: RangeBar should be in Overlay rather than Underlay

--- src/view/components/DopeSheetOverlay.tsx
+++ src/view/components/DopeSheetOverlay.tsx
@@ -1,17 +1,19 @@
 import React, { useRef } from 'react';
+import { RangeBar } from './RangeBar';
 import { Resolution } from '../utils/Resolution';
 import { State } from '../states/store';
 import { TimeValueLines } from './TimeValueLines';
+import { TimeValueRange } from '../utils/TimeValueRange';
 import styled from 'styled-components';
 import { useRect } from '../utils/useRect';
 import { useSelector } from 'react-redux';
 
 // == microcomponent ===============================================================================
-const Line = ( { size }: {
+const Line = ( { range, size }: {
+  range: TimeValueRange;
   size: Resolution;
 } ): JSX.Element => {
-  const { range, time } = useSelector( ( state: State ) => ( {
-    range: state.timeline.range,
+  const { time } = useSelector( ( state: State ) => ( {
     time: state.automaton.time
   } ) );
 
@@ -38,6 +40,13 @@ const Body = styled.div`
   height: calc( 100% - 4px );
 `;
 
+const StyledRangeBar = styled( RangeBar )`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 4px;
+`;
+
 const Root = styled.div`
 `;
 
@@ -50,16 +59,26 @@ const DopeSheetOverlay = ( props: DopeSheetOverlayProps ): JSX.Element => {
   const { className } = props;
   const refBody = useRef<HTMLDivElement>( null );
   const rect = useRect( refBody );
+  const { range, length } = useSelector( ( state: State ) => ( {
+    range: state.timeline.range,
+    length: state.automaton.length
+  } ) );
 
   return (
     <Root className={ className }>
       <Body ref={ refBody }>
         <SVGRoot>
           <Line
+            range={ range }
             size={ rect }
           />
         </SVGRoot>
       </Body>
+      <StyledRangeBar
+        range={ range }
+        width={ rect.width }
+        length={ length }
+      />
     </Root>
   );
 };
--- src/view/components/DopeSheetUnderlay.tsx
+++ src/view/components/DopeSheetUnderlay.tsx
@@ -1,6 +1,5 @@
 import React, { useRef } from 'react';
 import { Colors } from '../constants/Colors';
-import { RangeBar } from './RangeBar';
 import { State } from '../states/store';
 import { TimeValueGrid } from './TimeValueGrid';
 import styled from 'styled-components';
@@ -22,13 +21,6 @@ const Body = styled.div`
   background: ${ Colors.back1 };
 `;
 
-const StyledRangeBar = styled( RangeBar )`
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  height: 4px;
-`;
-
 const Root = styled.div`
 `;
 
@@ -41,9 +33,8 @@ const DopeSheetUnderlay = ( props: DopeSheetUnderlayProps ): JSX.Element => {
   const { className } = props;
   const refBody = useRef<HTMLDivElement>( null );
   const rect = useRect( refBody );
-  const { range, length } = useSelector( ( state: State ) => ( {
-    range: state.timeline.range,
-    length: state.automaton.length
+  const { range } = useSelector( ( state: State ) => ( {
+    range: state.timeline.range
   } ) );
 
   return (
@@ -56,11 +47,6 @@ const DopeSheetUnderlay = ( props: DopeSheetUnderlayProps ): JSX.Element => {
             hideValue
           />
         </SVGRoot>
-        <StyledRangeBar
-          range={ range }
-          width={ rect.width }
-          length={ length }
-        />
       </Body>
     </Root>
   );
