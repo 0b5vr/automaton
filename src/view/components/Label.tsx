b08cde875c3d5aad68c566dfebec9ce4f2ef6195
FMS-Cat <yutaka.obuchi@gmail.com>
2020-04-29 19:07:01 +0900

    (from automaton-with-gui repo) labels: add an ability to rename / delete labels

--- src/view/components/Label.tsx
+++ src/view/components/Label.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { TimeValueRange, t2x } from '../utils/TimeValueRange';
+import { useDispatch, useSelector } from '../states/store';
 import { Colors } from '../constants/Colors';
 import { Resolution } from '../utils/Resolution';
 import styled from 'styled-components';
@@ -29,6 +30,12 @@ const Label = ( { name, time, range, size }: {
   range: TimeValueRange;
   size: Resolution;
 } ): JSX.Element => {
+  const dispatch = useDispatch();
+  const {
+    automaton,
+  } = useSelector( ( state ) => ( {
+    automaton: state.automaton.instance,
+  } ) );
   const [ width, setWidth ] = useState( 0.0 );
   const x = t2x( time, range, size.width );
   const refText = useRef<SVGTextElement>( null );
@@ -42,8 +49,103 @@ const Label = ( { name, time, range, size }: {
     [ refText.current ]
   );
 
+  const renameLabel = useCallback(
+    ( x: number, y: number ): void => {
+      if ( !automaton ) { return; }
+
+      dispatch( {
+        type: 'TextPrompt/Open',
+        position: { x, y },
+        placeholder: 'A new name for the label',
+        checkValid: ( newName: string ) => {
+          if ( newName === '' ) { return 'Create Label: Name cannot be empty.'; }
+          if ( name === newName ) { return null; }
+          if ( automaton.labels[ newName ] != null ) { return 'Create Label: A label for the given name already exists.'; }
+          return null;
+        },
+        callback: ( newName ) => {
+          if ( name === newName ) { return; }
+
+          automaton.deleteLabel( name );
+          automaton.setLabel( newName, time );
+
+          dispatch( {
+            type: 'History/Push',
+            description: 'Rename Label',
+            commands: [
+              {
+                type: 'automaton/deleteLabel',
+                name,
+                timePrev: time
+              },
+              {
+                type: 'automaton/createLabel',
+                name: newName,
+                time
+              }
+            ],
+          } );
+        }
+      } );
+
+    },
+    [ automaton, time, range ]
+  );
+
+  const deleteLabel = useCallback(
+    (): void => {
+      if ( !automaton ) { return; }
+
+      automaton.deleteLabel( name );
+
+      dispatch( {
+        type: 'History/Push',
+        description: 'Delete Label',
+        commands: [
+          {
+            type: 'automaton/deleteLabel',
+            name,
+            timePrev: time
+          }
+        ],
+      } );
+    },
+    [ automaton, time, range ]
+  );
+
+  const handleContextMenu = useCallback(
+    ( event: React.MouseEvent ): void => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      const x = event.clientX;
+      const y = event.clientY;
+
+      dispatch( {
+        type: 'ContextMenu/Open',
+        position: { x, y },
+        commands: [
+          {
+            name: 'Rename Label',
+            description: 'Rename the label.',
+            callback: () => renameLabel( x, y )
+          },
+          {
+            name: 'Delete Label',
+            description: 'Delete the label.',
+            callback: () => deleteLabel()
+          }
+        ]
+      } );
+    },
+    [ renameLabel, deleteLabel ]
+  );
+
   return <>
-    <g transform={ `translate(${ x },${ size.height })` }>
+    <g
+      transform={ `translate(${ x },${ size.height })` }
+      onContextMenu={ handleContextMenu }
+    >
       <Line y2={ -size.height } />
       <Rect width={ width + 4 } height="12" y="-12" />
       <Text ref={ refText } x="2" y="-2">{ name }</Text>
