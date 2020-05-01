0abf3e7863fa97c34dcb0b1efcc9c9a4af4acd8a
FMS-Cat <yutaka.obuchi@gmail.com>
2020-05-02 01:43:23 +0900

    (from automaton-with-gui repo) improved grid generation

--- src/view/components/TimeValueGrid.tsx
+++ src/view/components/TimeValueGrid.tsx
@@ -2,6 +2,8 @@ import React, { useMemo } from 'react';
 import { TimeValueRange, t2x, v2y } from '../utils/TimeValueRange';
 import { Colors } from '../constants/Colors';
 import { Resolution } from '../utils/Resolution';
+import { clamp } from '../../utils/clamp';
+import { genGrid } from '../utils/genGrid';
 import styled from 'styled-components';
 
 // == styles =======================================================================================
@@ -35,67 +37,24 @@ const TimeValueGrid = ( props: TimeValueGridProps ): JSX.Element => {
 
   const hlines: GridLineEntry[] = useMemo(
     (): GridLineEntry[] => {
-      const delta = ( range.t1 - range.t0 );
-      const logDelta = Math.log10( delta );
-      const scale = Math.pow( 10.0, Math.floor( logDelta ) - 1.0 );
-      const lineInterval = logDelta - Math.floor( logDelta );
-      const num = Math.floor( range.t0 / scale );
-      const begin = num * scale;
-      let accent10 = num - Math.floor( num / 10 ) * 10;
-      let accent100 = num - Math.floor( num / 100 ) * 100;
-
-      const ret: GridLineEntry[] = [];
-      for ( let v = begin; v < range.t1; v += scale ) {
-        const opacity = (
-          accent100 === 0 ? 0.4 :
-          accent10 === 0 ? 0.4 - lineInterval * 0.3 :
-          0.1 - lineInterval * 0.3
-        );
-        if ( 0.0 < opacity ) {
-          ret.push( {
-            value: ( v + 1E-9 ).toFixed( 3 ), // trick: to prevent -0.000
-            position: t2x( v, range, size.width ),
-            opacity
-          } );
-        }
-        accent10 = ( accent10 + 1 ) % 10;
-        accent100 = ( accent100 + 1 ) % 100;
-      }
-
-      return ret;
+      const grid = genGrid( range.t0, range.t1, { details: 1 } );
+      return grid.map( ( entry ) => ( {
+        value: ( entry.value + 1E-9 ).toFixed( 3 ), // trick: to prevent -0.000
+        position: t2x( entry.value, range, size.width ),
+        opacity: clamp( entry.importance - 0.01, 0.0, 0.4 )
+      } ) );
     },
     [ range, size ]
   );
+
   const vlines: GridLineEntry[] = useMemo(
     (): GridLineEntry[] => {
-      const delta = ( range.v1 - range.v0 );
-      const logDelta = Math.log10( delta );
-      const scale = Math.pow( 10.0, Math.floor( logDelta ) - 1.0 );
-      const lineInterval = logDelta - Math.floor( logDelta );
-      const num = Math.floor( range.v0 / scale );
-      const begin = num * scale;
-      let accent10 = num - Math.floor( num / 10 ) * 10;
-      let accent100 = num - Math.floor( num / 100 ) * 100;
-
-      const ret: GridLineEntry[] = [];
-      for ( let v = begin; v < range.v1; v += scale ) {
-        const opacity = (
-          accent100 === 0 ? 0.4 :
-          accent10 === 0 ? 0.4 - lineInterval * 0.3 :
-          0.1 - lineInterval * 0.3
-        );
-        if ( 0.0 < opacity ) {
-          ret.push( {
-            value: ( v + 1E-9 ).toFixed( 3 ), // trick: to prevent -0.000
-            position: v2y( v, range, size.height ),
-            opacity
-          } );
-        }
-        accent10 = ( accent10 + 1 ) % 10;
-        accent100 = ( accent100 + 1 ) % 100;
-      }
-
-      return ret;
+      const grid = genGrid( range.v0, range.v1, { details: 1 } );
+      return grid.map( ( entry ) => ( {
+        value: ( entry.value + 1E-9 ).toFixed( 3 ), // trick: to prevent -0.000
+        position: v2y( entry.value, range, size.height ),
+        opacity: clamp( entry.importance - 0.01, 0.0, 0.4 )
+      } ) );
     },
     [ range, size ]
   );
