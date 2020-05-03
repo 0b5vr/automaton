1be45c822b1a23133bb9671baab9bcb270007dd2
FMS-Cat <yutaka.obuchi@gmail.com>
2020-05-03 23:27:05 +0900

    (from automaton-with-gui repo) playground: demo.js is done

--- playground-examples/demo.js
+++ playground-examples/demo.js
@@ -4,15 +4,15 @@
 const PI = Math.PI;
 const TAU = PI * 2.0;
 
-const width = canvas.width = 300;
-const height = canvas.height = 300;
+const width = canvas.width = 480;
+const height = canvas.height = 480;
 
-const context = canvas.getContext( "2d" );
+const context = canvas.getContext( '2d' );
 
 // == initialize an automaton instance =============================================================
 const automaton = new AUTOMATON_WITH_GUI.AutomatonWithGUI(
   JSON.parse( `
-  {"version":"3.0.0-beta3","resolution":100,"curves":[{"nodes":[{"value":0.2,"out":{"time":0.5,"value":0}},{"time":1,"value":0.6,"in":{"time":-0.5,"value":0}},{"time":1.1},{"time":2.2,"out":{"time":0.2029702970297027,"value":0}},{"time":2.683168316831683,"value":0.2,"in":{"time":-0.0058960808318827985,"value":-0.16171574263249228},"out":{"time":0.029151407919009283,"value":0.7995551137822344}},{"time":3.0792079207920793,"value":0.8,"in":{"time":-0.26237623762376217,"value":0}},{"time":4,"value":0.8,"out":{"time":0.5,"value":0}},{"time":5,"value":0.2,"in":{"time":-0.22277227722772253,"value":0.46032643646145566}}],"fxs":[{"def":"gravity","params":{"a":20,"e":0.5,"preserve":false},"time":1,"length":1.2},{"def":"cds","params":{"factor":1000,"ratio":0.15,"preserve":true},"time":2.2,"length":1.8},{"def":"sine","params":{"amp":0.2,"freq":2,"phase":0},"time":4,"length":1},{"def":"lofi","params":{"reso":10,"relative":true,"rate":20,"round":true},"time":4.5,"length":0.5,"row":1}]},{"nodes":[{},{"time":5,"value":1}]},{"nodes":[{"out":{"time":0.24111675126903553,"value":0}},{"time":1,"value":1,"in":{"time":-0.8147208121827411,"value":0}}]},{"nodes":[{"out":{"time":0.06345177664974619,"value":0}},{"time":0.2238578680203046,"value":0.8088235294117649,"in":{"time":-0.08883248730964469,"value":-0.08921568627450979},"out":{"time":0.08883248730964469,"value":0.08921568627450979}},{"time":1,"value":1,"in":{"time":-0.47969543147208127,"value":0}}]},{"nodes":[{"out":{"time":0.10913705583756345,"value":0}},{"time":0.6000000000000001,"value":1,"in":{"time":-0.5507614213197969,"value":0}}]},{"nodes":[{},{"time":0.1,"value":1},{"time":0.5,"value":1},{"time":0.6000000000000001},{"time":1.8}],"fxs":[{"def":"cds","params":{"factor":300,"ratio":1,"preserve":false},"length":0.5},{"def":"gravity","params":{"a":30,"e":0.5,"preserve":true},"time":0.5,"length":0.728136781314195}]},{"nodes":[{},{"time":0.5,"value":1,"in":{"time":-0.5,"value":0},"out":{"time":0.5,"value":0}},{"time":1}]}],"channels":{"ring/radiusInside":{"items":[{"length":1,"curve":2}]},"ring/radiusOutside":{"items":[{"length":1,"curve":3,"amp":0.9}]},"rect/x":{"items":[{"value":0.5}]},"rect/y":{"items":[{"value":0.5},{"time":0.4,"length":1.3,"value":0.5,"curve":5,"amp":-0.3}]},"rect/rotate":{"items":[{"time":0.2,"length":0.6,"curve":4}]},"rect/sizeX":{"items":[{"time":0.2,"length":0.6,"curve":4,"amp":0.29999999999999993}]},"rect/sizeY":{"items":[{"time":0.2,"length":0.6,"curve":4,"amp":0.29999999999999993}]},"rect/bounce":{"items":[{"time":0.8512232753726503,"value":1},{"time":1.7000000000000002}]}},"labels":{},"guiSettings":{"snapTimeActive":true,"snapTimeInterval":0.1,"snapValueActive":true,"snapValueInterval":0.1,"snapBeatActive":false,"snapBeatBPM":140,"minimizedPrecisionTime":3,"minimizedPrecisionValue":3}}
+{"version":"3.0.0-beta3","resolution":100,"curves":[{"nodes":[{"value":0.2,"out":{"time":0.5,"value":0}},{"time":1,"value":0.6,"in":{"time":-0.5,"value":0}},{"time":1.1},{"time":2.2,"out":{"time":0.2029702970297027,"value":0}},{"time":2.683168316831683,"value":0.2,"in":{"time":-0.0058960808318827985,"value":-0.16171574263249228},"out":{"time":0.029151407919009283,"value":0.7995551137822344}},{"time":3.0792079207920793,"value":0.8,"in":{"time":-0.26237623762376217,"value":0}},{"time":4,"value":0.8,"out":{"time":0.5,"value":0}},{"time":5,"value":0.2,"in":{"time":-0.22277227722772253,"value":0.46032643646145566}}],"fxs":[{"def":"gravity","params":{"a":20,"e":0.5,"preserve":false},"time":1,"length":1.2},{"def":"cds","params":{"factor":1000,"ratio":0.15,"preserve":true},"time":2.2,"length":1.8},{"def":"sine","params":{"amp":0.2,"freq":2,"phase":0},"time":4,"length":1},{"def":"lofi","params":{"reso":10,"relative":true,"rate":20,"round":true},"time":4.5,"length":0.5,"row":1}]},{"nodes":[{},{"time":5,"value":1}]},{"nodes":[{"out":{"time":0.24111675126903553,"value":0}},{"time":1,"value":1,"in":{"time":-0.8147208121827411,"value":0}}]},{"nodes":[{"out":{"time":0.06345177664974619,"value":0}},{"time":0.2238578680203046,"value":0.8088235294117649,"in":{"time":-0.08883248730964469,"value":-0.08921568627450979},"out":{"time":0.08883248730964469,"value":0.08921568627450979}},{"time":1,"value":1,"in":{"time":-0.47969543147208127,"value":0}}]},{"nodes":[{"out":{"time":0.10913705583756345,"value":0}},{"time":0.6000000000000001,"value":1,"in":{"time":-0.5507614213197969,"value":0}}]},{"nodes":[{},{"time":0.1,"value":1},{"time":0.5,"value":1},{"time":0.6000000000000001},{"time":1.8}],"fxs":[{"def":"cds","params":{"factor":300,"ratio":1,"preserve":false},"length":0.5},{"def":"gravity","params":{"a":30,"e":0.5,"preserve":true},"time":0.5,"length":0.728136781314195}]},{"nodes":[{},{"time":0.5,"value":1,"in":{"time":-0.5,"value":0},"out":{"time":0.5,"value":0}},{"time":1}]},{"nodes":[{"value":1,"out":{"time":0.05,"value":0}},{"time":0.05,"in":{"time":-0.05,"value":0}}]},{"nodes":[{},{"time":1,"value":1}]},{"nodes":[{"out":{"time":0.2817258883248731,"value":0}},{"time":0.4,"value":1,"in":{"time":-0.11421319796954314,"value":-0.5970588235294118}}]}],"channels":{"rect/x":{"items":[{"value":0.5}]},"rect/y":{"items":[{"value":0.5},{"time":0.4,"length":1.3000000000000003,"value":0.5,"curve":5,"amp":-0.3},{"time":3.6,"length":0.40000000000000036,"value":0.5,"curve":9}]},"rect/rotate":{"items":[{"time":0.2,"length":0.6,"curve":4},{"time":3.6,"length":0.40000000000000036,"curve":9,"amp":0.1}]},"rect/sizeX":{"items":[{"time":0.2,"length":0.6,"curve":4,"amp":0.29999999999999993},{"time":2,"length":0.15,"value":0.3,"curve":4,"speed":4,"amp":0.6000000000000001}]},"rect/sizeY":{"items":[{"time":0.2,"length":0.6,"curve":4,"amp":0.29999999999999993}]},"rect/bounce":{"items":[{"time":0.9,"length":0.8000000000000002,"value":1,"reset":true}]},"ring/radius0":{"items":[{"length":1,"curve":3}]},"ring/radius1":{"items":[{"time":0.045685279187817264,"length":1,"curve":3,"amp":0.9}]},"ring/radius2":{"items":[{"time":0.10152284263959391,"length":1,"curve":3,"amp":0.9}]},"ring/radius3":{"items":[{"time":0.15228426395939088,"length":1,"curve":3,"amp":0.9}]},"ring/radius4":{"items":[{"time":0.1979695431472081,"length":1,"curve":3,"amp":1.1}]},"dots/radius0":{"items":[{"time":1.609375,"length":0.15,"curve":4,"speed":4,"amp":0.05}]},"dots/radius1":{"items":[{"time":1.625,"length":0.15,"curve":4,"speed":4,"amp":0.05}]},"dots/radius2":{"items":[{"time":1.65625,"length":0.15,"curve":4,"speed":4,"amp":0.05}]},"dots/radius3":{"items":[{"time":1.640625,"length":0.15,"curve":4,"speed":4,"amp":0.05}]},"rect/fill":{"items":[{"value":1},{"time":1.8,"length":0.05,"curve":7},{"time":1.85,"length":0.05,"curve":7},{"time":1.9000000000000001,"length":0.05,"curve":7}]},"text/name":{"items":[{"time":2.1,"length":0.30000000000000027,"curve":8,"speed":3.33333333333334}]},"text/description":{"items":[{"time":2.5,"length":0.8,"curve":8,"speed":1.25}]}},"labels":{},"guiSettings":{"snapTimeActive":true,"snapTimeInterval":0.1,"snapValueActive":true,"snapValueInterval":0.1,"snapBeatActive":false,"snapBeatBPM":120,"minimizedPrecisionTime":3,"minimizedPrecisionValue":3}}
   ` ), // put your automaton savedata here
   {
     gui: divAutomatonContainer, // where you want to put entire automaton GUI
@@ -62,41 +62,32 @@ function drawBackground() {
   context.fillRect( 0, 0, width, height );
 }
 
-function drawRing() {
-  const radiusInside = 0.5 * height * auto( 'ring/radiusInside' );
-  const radiusOutside = 0.5 * height * auto( 'ring/radiusOutside' );
-
-  if ( radiusOutside <= radiusInside ) { return; }
+function drawRing( channel, color ) {
+  const radius = 0.5 * height * auto( channel );
 
   context.save();
   context.translate( 0.5 * width, 0.5 * height );
-  context.fillStyle = "#d0edff";
+  context.fillStyle = color;
 
   context.beginPath();
-  context.ellipse( 0.0, 0.0, radiusInside, radiusInside, 0.0, 0.0, TAU, true );
-  context.ellipse( 0.0, 0.0, radiusOutside, radiusOutside, 0.0, 0.0, TAU, false );
+  context.ellipse( 0.0, 0.0, radius, radius, 0.0, 0.0, TAU, false );
   context.fill();
 
   context.restore();
 }
 
 let rectPrevY = 0.0;
-function drawRect() {
+function transRect( fn ) {
   const x = auto( 'rect/x' );
   const y = auto( 'rect/y' );
-  const sizeX = auto( 'rect/sizeX' );
-  const sizeY = auto( 'rect/sizeY' );
   const rotate = TAU * auto( 'rect/rotate' );
   const bounce = auto( 'rect/bounce' );
 
   const deltaY = ( y - rectPrevY ) / deltaTime;
   rectPrevY = y;
 
-  if ( sizeX <= 0.0 || sizeY <= 0.0 ) { return; }
-
   context.save();
   context.translate( width * x, height * y );
-  context.scale( width * sizeX, height * sizeY );
 
   if ( bounce ) {
     context.translate( 0.0, 1.0 );
@@ -105,9 +96,95 @@ function drawRect() {
   }
 
   context.rotate( rotate );
-  context.fillStyle = "#d0edff";
 
+  fn();
+
+  context.restore();
+}
+
+function drawRect() {
+  const sizeX = auto( 'rect/sizeX' );
+  const sizeY = auto( 'rect/sizeY' );
+  const fill = auto( 'rect/fill' );
+
+  if ( sizeX <= 0.0 || sizeY <= 0.0 ) { return; }
+
+  context.save();
+
+  context.scale( width * sizeX, height * sizeY );
+  context.fillStyle = '#d0edff';
   context.fillRect( -0.5, -0.5, 1.0, 1.0 );
+
+  context.restore();
+
+  context.save();
+
+  context.scale( width * ( sizeX - 0.02 ), height * ( sizeY - 0.02 ) );
+  context.fillStyle = '#191a1f';
+  context.globalAlpha = 1.0 - fill;
+  context.fillRect( -0.5, -0.5, 1.0, 1.0 );
+
+  context.restore();
+}
+
+function drawDots() {
+  const rectSizeX = auto( 'rect/sizeX' );
+  const rectSizeY = auto( 'rect/sizeY' );
+
+  for ( let i = 0; i < 4; i ++ ) {
+    const radius = 0.5 * height * auto( 'dots/radius' + i );
+    const radiusIn = Math.max( 0.0, radius - height * 0.01 );
+
+    if ( radius <= 0.0 ) { continue; }
+
+    const x = ( width * ( rectSizeX - 0.01 ) ) * ( ( i % 2 ) - 0.5 );
+    const y = ( height * ( rectSizeY - 0.01 ) ) * ( Math.floor( i / 2 ) - 0.5 );
+
+    context.save();
+    context.translate( x, y );
+
+    context.fillStyle = '#00aaff';
+    context.beginPath();
+    context.ellipse( 0.0, 0.0, radius, radius, 0.0, 0.0, TAU, false );
+    context.fill();
+
+    context.fillStyle = '#191a1f';
+    context.beginPath();
+    context.ellipse( 0.0, 0.0, radiusIn, radiusIn, 0.0, 0.0, TAU, false );
+    context.fill();
+
+    context.restore();
+  }
+}
+
+function truncateText( text, channel ) {
+  const value = auto( channel );
+  if ( value <= 0.0 ) { return ''; }
+  if ( 1.0 <= value ) { return text; }
+  return text.substring( 0, Math.floor( text.length * value ) ) + '|';
+}
+
+function drawText() {
+  const rectSizeX = auto( 'rect/sizeX' );
+  const rectSizeY = auto( 'rect/sizeY' );
+  
+  context.save();
+  context.translate(
+    -width * ( 0.5 * rectSizeX - 0.02 ),
+    height * ( 0.5 * rectSizeY - 0.03 )
+  );
+  
+  const textDescription = truncateText( 'Animation engine for creative coding', 'text/description' );
+  context.font = `${ height * 0.05 }px Roboto`;
+  context.fillStyle = '#d0edff';
+  context.fillText( textDescription, 0, 0 );
+
+  context.translate( 0.0, -height * 0.05 );
+  
+  const textName = truncateText( 'Automaton', 'text/name' );
+  context.font = `${ height * 0.13 }px Roboto`;
+  context.fillStyle = '#d0edff';
+  context.fillText( textName, 0, 0 );
 
   context.restore();
 }
@@ -119,6 +196,15 @@ playground.update = () => {
   automaton.update( time );
 
   drawBackground();
-  drawRing();
-  drawRect();
+  drawRing( 'ring/radius0', '#d0edff' );
+  drawRing( 'ring/radius1', '#f7f025' );
+  drawRing( 'ring/radius2', '#ff5a1f' );
+  drawRing( 'ring/radius3', '#ff0066' );
+  drawRing( 'ring/radius4', '#191a1f' );
+
+  transRect( () => {
+    drawRect();
+    drawText();
+    drawDots();
+  } );
 };
