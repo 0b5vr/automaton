d651d89ef9f9e97d6751776089e8b97e95f63840
FMS-Cat <yutaka.obuchi@gmail.com>
2020-05-06 22:55:19 +0900

    (from automaton-with-gui repo) add an example: getting-started.js

--- playground-examples/getting-started.js
+++ playground-examples/getting-started.js
@@ -1,59 +1,65 @@
-// Press Ctrl+S (Cmd+S on MacOS) to run script
-// The script will be run under 🔥 eval() 🔥. be calm, safety first.
+// Welcome to the Automaton tutorial!
+// I'm going to introduce you how to use Automaton.
 
-const width = canvas.width = 300;
-const height = canvas.height = 300;
+// Automaton is an animation engine for creative coding.
+// It also comes with a GUI timeline editor!
+// I hope it helps your creative coding!
+// Automaton is still under development so some features will be added / changed in future.
 
-const ctx = canvas.getContext( "2d" );
+// == initialize an Automaton instance =============================================================
 
-let isHalted = false;
+// First, you have to initialize an Automaton instance.
+// The constructor of Automaton receives two parameters: animation data and options.
+// In the Automaton world, all of animation data is stored in a JSON object.
+// You can save and load your animation you modified on the GUI using JSON.
+// You can also specifiy several options too.
 
-// == helpers ======================================================================================
-function loadImage( src ) {
-  return new Promise( ( resolve, reject ) => {
-    const image = new Image();
-    image.onload = () => resolve( image );
-    image.onerror = () => reject();
-    image.src = src;
-  } );
-}
-
-// == load images ==================================================================================
-const imageAutomaton = await loadImage( 'https://raw.githubusercontent.com/FMS-Cat/automaton-with-gui/v3.0.0-beta3/src/view/icons/automaton-a.svg' );
-
-// == initialize an automaton instance =============================================================
 const automaton = new AUTOMATON_WITH_GUI.AutomatonWithGUI(
   JSON.parse( `
-{"v":"2.0.0","length":5,"resolution":100,"params":{"rectX":{"nodes":[{"time":0,"value":0.2,"out":{"time":0.5,"value":0}},{"time":1,"value":0.6,"in":{"time":-0.5,"value":0},"out":{"time":0,"value":0}},{"time":1.1,"value":0,"in":{"time":0,"value":0},"out":{"time":0,"value":0}},{"time":2.2,"value":0,"in":{"time":0,"value":0},"out":{"time":0.2029702970297027,"value":0}},{"time":2.683168316831683,"value":0.2,"in":{"time":-0.0058960808318827985,"value":-0.16171574263249228},"out":{"time":0.029151407919009283,"value":0.7995551137822344}},{"time":3.0792079207920793,"value":0.8,"in":{"time":-0.26237623762376217,"value":0},"out":{"time":0,"value":0}},{"time":4,"value":0.8,"in":{"time":0,"value":0},"out":{"time":0.5,"value":0}},{"time":5,"value":0.2,"in":{"time":-0.22277227722772253,"value":0.46032643646145566}}],"fxs":[{"time":1,"length":1.2,"row":0,"def":"gravity","params":{"a":20,"e":0.5,"preserve":false},"bypass":false},{"time":2.2,"length":1.8,"row":0,"def":"cds","params":{"factor":1000,"ratio":0.15,"preserve":true},"bypass":false},{"time":4,"length":1,"row":0,"def":"sine","params":{"amp":0.2,"freq":2,"phase":0}},{"def":"lofi","bypass":false,"row":1,"time":4.5,"length":0.5,"params":{"reso":10,"relative":true,"rate":20,"round":true}}]},"rectY":{"nodes":[{"time":0,"value":0,"out":{"time":0,"value":0}},{"time":5,"value":1,"in":{"time":0,"value":0}}],"fxs":[]}},"guiSettings":{"snapActive":false,"snapTime":0.1,"snapValue":0.1}}
+{"version":"3.0.0-beta3","resolution":100,"curves":[{"nodes":[{"out":{"time":1,"value":0}},{"time":2,"value":1,"in":{"time":-1,"value":0}}]}],"channels":{"rectX":{"items":[{},{"time":1,"value":0.75},{"time":1.5,"value":0.25}]},"rectY":{"items":[{"length":2,"curve":0}]}},"labels":{},"guiSettings":{"snapTimeActive":false,"snapTimeInterval":0.1,"snapValueActive":false,"snapValueInterval":0.1,"snapBeatActive":false,"snapBeatBPM":140,"minimizedPrecisionTime":3,"minimizedPrecisionValue":3}}
   ` ), // put your automaton savedata here
   {
-    gui: divAutomatonContainer, // where you want to put entire automaton GUI
+    gui: divAutomatonContainer, // where you want to put entire Automaton GUI
     isPlaying: true, // it's playing by default
     installBuiltinFxs: true, // install bunch of fxs by default
   }
 );
+
+// Here we are going to define a function called `auto`.
+// The function auto is what the Automaton ties our code with animations!
+// We will introduce you how do we use this later.
+
 const auto = automaton.auto; // mighty auto function!
 
 // == define clock =================================================================================
+
+// We need to define a time in order to use Automaton.
+// If you already have a time on your framework, you can use them instead.
+// Here we are going define a simple clock.
+
 let prevDate = Date.now();
 let time = 0.0;
+let deltaTime = 0.0;
 let isPaused = false;
 
 function updateClock() {
   const now = Date.now();
   if ( !isPaused ) {
-    const delta = now - prevDate;
-    time += delta * 0.001;
+    deltaTime = 0.001 * ( now - prevDate );
+    time += deltaTime;
   }
   prevDate = now;
 
   if ( automaton.length <= time ) {
-    automaton.reset(); // we should call this when we seek the time
+    automaton.reset(); // we should call this when we modify the time backwards
     time = time % automaton.length;
   }
 }
 
-// time controls -- if we don't define them, the play-pause button and seekbar don't do anything
+// Now we have to connect our time into controls of Automaton GUI.
+// We do that by subscribing to Automaton's `play`, `pause`, and `seek` events.
+// If we don't subscribe them, the play-pause button and seekbar don't do anything.
+
 automaton.on( 'play', () => {
   isPaused = false;
 } );
@@ -64,43 +70,62 @@ automaton.on( 'pause', () => {
 
 automaton.on( 'seek', ( event ) => {
   time = event.time;
-  automaton.reset(); // we should call this when we seek the time
+  automaton.reset(); // we should call this when we modify the time backwards
 } );
 
-// == update loop ==================================================================================
-function update() {
-  if ( isHalted ) { return; }
+// == canvas =======================================================================================
 
-  // update it
+// We are going to create a program that simply shows a rect using canvas API.
+// The rect is gonna move based on an animation we define in Automaton!
+
+// An entity called Channel is very crucial.
+// It represents a single numeric value in Automaton world.
+// You can put constants or curves onto the curve in order to animate the value of the Channel.
+// Now we are going to move a rect using two Channels: `rectX` and `rectY` .
+
+const width = canvas.width = 320;
+const height = canvas.height = 320;
+
+const context = canvas.getContext( '2d' );
+
+function drawRect() {
+
+  // Background; nothing particular.
+  context.fillStyle = '#ffffff';
+  context.fillRect( 0, 0, width, height );
+
+  // Here we use the `auto` function.
+  // It simply receives a string that indicates a name of channel and returns a channel's value!
+  // Note that you have to call the auto function in update loop everytime or the value is not updated.
+  const rectX = auto( 'rectX' );
+  const rectY = auto( 'rectY' );
+
+  // Now draw the rect on the canvas using the value we got.
+  context.fillStyle = '#ff0000';
+  context.fillRect( 100.0 * rectX, 100.0 * rectY, 100.0, 100.0 );
+
+}
+
+// == update loop ==================================================================================
+
+playground.update = () => { // `playground.update` is a simple per-frame call
+
+  // update the time
   updateClock();
+
+  // Automaton needs to be updated per frame in your update loop!
+  // Just give a current time in order to make it work.
   automaton.update( time );
 
-  ctx.fillStyle = "#fff";
-  ctx.fillRect( 0, 0, width, height );
+  // Now we are going to draw the rect.
+  drawRect();
 
-  ctx.fillStyle = "#f00";
+};
 
-  // here's the auto function!
-  ctx.fillRect(
-    auto( "rectX" ) * 100,
-    auto( "rectY" ) * 100,
-    100,
-    100
-  );
+// == unload procedure =============================================================================
 
-  requestAnimationFrame( update );
-}
+// We should unmount the GUI when we leave the playground.
 
-// == init =========================================================================================
-async function init() {
-  await Promise.all( [
-    imageAutomaton
-  ] );
-  update();
-}
-init();
-
-// == destroying procedure =========================================================================
 playground.unload = () => {
-  isHalted = true;
+  automaton.unmountGUI();
 };
