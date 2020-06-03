e4a6e99f6fe1090efc87b9ac529110c3208770c1
FMS-Cat <yutaka.obuchi@gmail.com>
2020-06-03 20:47:12 +0900

    (from automaton-with-gui repo) honestly I prefer even wider one

--- src/view/constants/Metrics.ts
+++ src/view/constants/Metrics.ts
@@ -1,9 +1,9 @@
 export const Metrics = {
   rootFontSize: 16,
   headerHeight: 32,
-  channelListWidth: 128,
+  channelListWidth: 160,
   inspectorWidth: 192,
-  curveListWidth: 128,
+  curveListWidth: 160,
   modeSelectorWidth: 36,
   curveListEntryHeight: 36,
   toastyWidth: 320
