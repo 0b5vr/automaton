7fa0dfd50cc285e08e93fada7644f23fe6c65fbc
FMS-Cat <yutaka.obuchi@gmail.com>
2020-09-01 01:46:43 +0900

    (from automaton-fxs repo) init

---
+++ src/fxs/exp.ts
@@ -0,0 +1,20 @@
+import type { FxDefinition } from '@fms-cat/automaton';
+
+export const exp: FxDefinition = {
+  name: 'Exponential Smoothing',
+  description: 'Smooth the curve. Simple but good.',
+  params: {
+    factor: { name: 'Factor', type: 'float', default: 10.0, min: 0.0 }
+  },
+  func( context ) {
+    const v = context.value;
+
+    if ( context.init ) {
+      context.state.pos = v;
+    }
+
+    const k = Math.exp( -context.deltaTime * context.params.factor );
+    context.state.pos = context.state.pos * k + v * ( 1.0 - k );
+    return context.state.pos;
+  }
+};
