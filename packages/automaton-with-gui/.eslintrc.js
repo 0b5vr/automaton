d956c25efb7984e9ca52f3e342f7cdd830d27032
FMS-Cat <yutaka.obuchi@gmail.com>
2020-11-07 20:58:18 +0900

    chore (lint): merge a rule from with-gui into root

--- packages/automaton-with-gui/.eslintrc.js
+++ packages/automaton-with-gui/.eslintrc.js
@@ -17,8 +17,5 @@ module.exports = {
         "react/display-name": [ "error" ], // yes please, it's very important to do performance monitoring
         "react-hooks/rules-of-hooks": [ "error" ], // yes it must be followed
         "react-hooks/exhaustive-deps": [ "warn" ], // best eslint rule ever
-
-        // typescript-specifics
-        "@typescript-eslint/explicit-module-boundary-types": [ "off" ], // We are using explicit any on purpose because it's explicit, be permissive
     }
 };
