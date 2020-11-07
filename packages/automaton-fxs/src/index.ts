67a669e4b091c64f27e782b8c8e5b534d6be3a8a
FMS-Cat <yutaka.obuchi@gmail.com>
2020-11-07 19:08:38 +0900

    feat (fxs): add an fx, hermitePatch

--- packages/automaton-fxs/src/index.ts
+++ packages/automaton-fxs/src/index.ts
@@ -3,6 +3,7 @@ export { cds } from './fxs/cds';
 export { clamp } from './fxs/clamp';
 export { exp } from './fxs/exp';
 export { gravity } from './fxs/gravity';
+export { hermitePatch } from './fxs/hermitePatch';
 export { lofi } from './fxs/lofi';
 export { noise } from './fxs/noise';
 export { pow } from './fxs/pow';
