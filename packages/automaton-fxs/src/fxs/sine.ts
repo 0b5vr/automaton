e19b90095abdbb4eddfc7a476f08f5526499df58
FMS-Cat <yutaka.obuchi@gmail.com>
2020-11-10 20:43:57 +0900

    change (fx): update sine (BREAKING!!)

--- packages/automaton-fxs/src/fxs/sine.ts
+++ packages/automaton-fxs/src/fxs/sine.ts
@@ -8,11 +8,11 @@ export const sine: FxDefinition = {
   params: {
     amp: { name: 'Amp', type: 'float', default: 0.1 },
     freq: { name: 'Frequency', type: 'float', default: 5.0 },
-    phase: { name: 'Phase', type: 'float', default: 0.0, min: 0.0, max: 1.0 }
+    offset: { name: 'Offset', type: 'float', default: 0.0, min: 0.0, max: 1.0 }
   },
   func( context ) {
     const v = context.value;
-    const p = context.progress * context.params.freq + context.params.phase;
+    const p = context.elapsed * context.params.freq + context.params.offset;
     return v + context.params.amp * Math.sin( p * TAU );
   }
 };
