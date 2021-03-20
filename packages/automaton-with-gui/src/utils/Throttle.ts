e0457f020e9524448d3f37a612d17c09eb69311a
FMS-Cat <yutaka.obuchi@gmail.com>
2021-03-20 20:48:34 +0900

    performance (GUI): throttle previewTime updates

---
+++ packages/automaton-with-gui/src/utils/Throttle.ts
@@ -0,0 +1,34 @@
+export class Throttle<T = void> {
+  /**
+   * Rate, in ms.
+   */
+  public rate;
+
+  private __currentPromise: Promise<T> | null = null;
+  private __latest: ( () => T ) | null = null;
+
+  private __lastTime = 0;
+
+  public constructor( rate?: number ) {
+    this.rate = rate ?? 100;
+  }
+
+  public do( func: () => T ): Promise<T> {
+    this.__latest = func;
+
+    const now = Date.now();
+    const untilNext = Math.max( 0, this.__lastTime + this.rate - now );
+
+    if ( !this.__currentPromise ) {
+      this.__currentPromise = new Promise( ( resolve ) => {
+        setTimeout( () => {
+          this.__lastTime = Date.now();
+          resolve( this.__latest!() );
+          this.__currentPromise = null;
+        }, untilNext );
+      } );
+    }
+
+    return this.__currentPromise;
+  }
+}
