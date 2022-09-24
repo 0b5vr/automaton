53d2d8ac9dc2d029430552c3b514a3d64ff945ee
0b5vr <0b5vr@0b5vr.com>
2022-09-24 11:12:52 +0900

    change: minimizeData, don't save repeat when it's nor required

--- packages/automaton-with-gui/src/minimizeData.ts
+++ packages/automaton-with-gui/src/minimizeData.ts
@@ -18,7 +18,8 @@ function minimizeChannelItem(
   const time = precOrUndefined( data.time, options.precisionTime );
   const length = precOrUndefined( data.length, options.precisionTime );
   const value = precOrUndefined( data.value, options.precisionValue );
-  const repeat = precOrUndefined( data.repeat, options.precisionTime );
+  let repeat = precOrUndefined( data.repeat, options.precisionTime );
+  repeat = repeat != null && 0.0 < repeat && length != null && repeat < length ? length : undefined;
   const reset = data.reset ? true : undefined;
   const curve = data.curve;
   const speed = precOrUndefined( data.speed, options.precisionTime );
